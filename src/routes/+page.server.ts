004fd090f8efa7011eacb8057e8dd25c66f8fe79
André devkcud Albanese Junior <patommmmm@proton.me>
2024-02-23 20:07:11 -0300

    Adiciona botão de debug para adicionar livros template

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,64 @@
+import { redirect } from '@sveltejs/kit';
+import type { Actions } from './$types';
+
+export const actions: Actions = {
+  addbooks: async () => {
+    const books = [
+      {
+        title: 'The Lord of the Rings',
+        synopsis: 'A very long book',
+        author: 'J.R.R. Tolkien',
+        publisher: 'HarperCollins',
+        pages: 500,
+      },
+      {
+        title: 'The Hobbit',
+        synopsis: 'A very long book',
+        author: 'J.R.R. Tolkien',
+        publisher: 'HarperCollins',
+        pages: 500,
+      },
+      {
+        title: 'The Fellowship of the Ring',
+        synopsis: 'A very long book',
+        author: 'J.R.R. Tolkien',
+        publisher: 'HarperCollins',
+        pages: 500,
+      },
+      {
+        title: 'The Two Towers',
+        synopsis: 'A very long book',
+        author: 'J.R.R. Tolkien',
+        publisher: 'HarperCollins',
+        pages: 500,
+      },
+      {
+        title: 'The Return of the King',
+        synopsis: 'A very long book',
+        author: 'J.R.R. Tolkien',
+        publisher: 'HarperCollins',
+        pages: 500,
+      },
+      {
+        title: 'The Silmarillion',
+        synopsis: 'A very long book',
+        author: 'J.R.R. Tolkien',
+        publisher: 'HarperCollins',
+        pages: 500,
+      },
+      {
+        title: 'The Children of Hurin',
+        synopsis: 'A very long book',
+        author: 'J.R.R. Tolkien',
+        publisher: 'HarperCollins',
+        pages: 500,
+      }
+    ];
+
+    for (const book of books) {
+      await prisma.book.create({ data: book });
+    }
+
+    throw redirect(302, '/');
+  }
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,4 +3,8 @@
   <p>Estamos preparando sua experiência. Pedimos que aguarde...</p>
 
   <a class="btn btn-primary" href="/livros">Ver livros</a>
+
+  <form action="?/addbooks" method="POST">
+    <button type="submit" class="btn btn-error">DEBUG: Adicionar livros</button>
+  </form>
 </div>
